337ae42fced17c5a687b075a6decbb7b472bb5ba
“Douglas <douglas.conceicao@taking.com.br>
2024-06-15 10:35:09 -0300

    feat: create entities

---
+++ src/main/java/com/events/api/domain/address/Address.java
@@ -0,0 +1,34 @@
+package com.events.api.domain.address;
+
+import com.events.api.domain.event.Event;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name="address")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name="event_id")
+    private Event event;
+}
